d66358846a372486b3cc13a436958e32a59bbfb8
Brian Tung <inb4ohnoes@me.com>
2014-07-03 21:11:58 -0400

    Initial Commit

---
+++ PocketSaver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PocketSaver
+//
+//  Created by Brian Tung on 7/3/14.
+//  Copyright (c) 2014 Flame Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PocketSaver/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PocketSaver
+//
+//  Created by Brian Tung on 7/3/14.
+//  Copyright (c) 2014 Flame Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
